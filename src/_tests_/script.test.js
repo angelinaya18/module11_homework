a3020a5c5e7c49c68d01c352c62e1fbf27ef0ea2
Angelina <88667402+angelinaya18@users.noreply.github.com>
2023-03-30 14:03:17 +0500

    Add files via upload

--- src/_tests_/script.test.js
+++ src/_tests_/script.test.js
@@ -1,11 +1,13 @@
 import { getMonth } from '../script.js';
 
 describe('getting the name of the month by number',()=>
+    //тест на корретные значения месяца (>1 и <12)
     it('correct number',()=>{
         expect(getMonth(1)).toBe('январь');
         expect(getMonth(5)).toBe('май');
         expect(getMonth(12)).toBe('декабрь');
     }),
+    //тест на некорретные значения месяца (<1 или >12)
     it('invalid number',()=>{
         expect(getMonth(0)).toBe('некорретное значение');
         expect(getMonth(13)).toBe('некорретное значение');
